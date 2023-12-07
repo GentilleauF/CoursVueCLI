f12cac9f6a3d7bf581dba2dde6853e68376f39d0
GentilleauF <francois.gentilleau@gmail.com>
2023-12-07 16:09:51 +0100

    push

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <!-- <div class="container"> -->
+    <h1 class="text-center">Les Amis</h1>
+    <ul class="list-group">
+      <un-ami name='jo' phone="05256255" email="ndedeonfe" premium="1"></un-ami>
+      <un-ami name='tom' phone="056255" email="noezdonfe" premium="0"></un-ami>
+
+    </ul>
+  <!-- </div> -->
+</template>
+
+<script>
+export default{
+  data(){
+    return {
+      lesAmis: [
+        {
+            id: 'lasticot',
+            name: 'COCO L ASTICOT',
+            phone: '01234 5678 991',
+            email: 'coco@lasticot.com',
+        },
+        {
+            id: 'janine',
+            name: 'Janine DeLavega',
+            phone: '09876 543 221',
+            email: 'janine@delavega.com',
+        },
+    ],
+    }
+  },
+}
+</script>
---
+++ src/components/UnAmi.vue
@@ -0,0 +1,47 @@
+<script>
+
+export default {
+    props : ["name", "phone", "email", "premium"],
+    data(){
+        return{
+            detailsVisibles:false,
+            unPote:{
+                id:'jojo',
+                name:"jojoLeBarjo",
+                phone:'123 12346 24',
+                email:'jojo@barjo.com',
+            },
+            premiumData: this.premium
+        }
+    },
+    methods:{
+        afficherDetails(){
+            this.detailsVisibles = !this.detailsVisibles;
+        },
+
+        afficherPremium() {
+            if (this.premiumData == 1) {
+                this.premiumData =0
+            } else {
+                this.premiumData = 1
+            }
+        }
+    },
+}
+</script>
+
+
+<template>
+    <div class="container my-1">
+        <ul class="list-group">
+            <h2 class="list-group-item">{{name}}</h2> 
+            <h2 class="list-group-item">{{premiumData==1 ? "Ami Premium" : "Ami nul"}}</h2> 
+            <button @click="afficherPremium" class="btn btn-danger" >Premium</button>
+            <button @click="afficherDetails" class="btn btn-primary">Voir Détails</button>           
+            <ul v-if="detailsVisibles" class="list-group">
+                <li class="list-group-item">{{phone}}</li>
+                <li class="list-group-item">{{email}}</li>
+            </ul>
+        </ul>
+    </div> 
+</template>
